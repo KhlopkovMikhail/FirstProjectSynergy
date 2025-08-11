2b50491ba84504cbf567f2079aa8ee1ae67f8601
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-08-11 23:19:09 +0500

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,19 @@
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        // Решение квадратного уравнения
+        // a*x + b*x*x + c = 0
+        // 5 - 2 - 3 = 0
+        int a = 5;
+        int b = -2;
+        int c = -3;
+        int x;
+        int y;
+        int D = b*b - 4*a*c;
+        // a*x + b*x*x + c
+        double x1 = ( -b + Math.sqrt(D)) / (2*a);
+        double x2 = ( -b - Math.sqrt(D)) / (2*a);
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+}
